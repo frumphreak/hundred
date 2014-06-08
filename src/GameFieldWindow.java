dfa8c3234e7bc452d8938de2cd01bb7b63f254f8
raspulitel <mars_kair@mail.ru>
2014-06-08 13:13:25 +0300

    Логіка вирізана і перенесена в LogicMainWindow (трохи криво)

--- src/GameFieldWindow.java
+++ src/GameFieldWindow.java
@@ -16,20 +16,6 @@ class GameFieldWindow extends JFrame implements ActionListener {
     // кнопок
     static JButton[] arrayButton = new JButton[razmerMassiva];// Визначенний
     // масив кнопок
-    String nameOfButton;
-    static int[] nArr = new int[razmerMassiva];
-    static int button1 = 9000, button2 = 9000;// будь-яке число, що перевищує
-    // потенційний розмір масиву.
-	/*
-	 * button1 зберігає індекс першої НАТИСНУТОЇ кнопки в ОДНОМІРНОМУ масиві.
-	 * button2 по аналогії для другої натиснутої кнопки.
-	 */
-
-    int IndexDiff = 0;// button2-button1
-    int bigger, smaller, temp;
-
-    int jobCountNajatiy = 0, previousI = 0;
-    int buffer[] = new int[2];// зберігає конкретне значення масиву
 
     public GameFieldWindow() {
         super("Віконечко");
@@ -37,152 +23,9 @@ class GameFieldWindow extends JFrame implements ActionListener {
     }
 
 
-    // Обробка натискання клавіш
-    public void actionPerformed(ActionEvent actionEvent) {
-
-        // Обробка 181 кнопки
-        int countButton = 0;
-        boolean zakreslennya = false;
-        int prevIndexI = 0, prevIndexJ = 0, IndexI = 0, IndexJ = 0;// значення
-        // індексів
-        // i and j
-        boolean knopkyNajali = false;
-        String msg;
-
-        // Обробка натискання клавіш вікна
-        for (int i = 0; i < razmerMassiva; i++) {
-            if (actionEvent.getSource() == arrayButton[i]) {
-                jobCountNajatiy++;
-                if (jobCountNajatiy == 1) {
-                    countButton = 0;
-                }// перше натискання
-                if (jobCountNajatiy == 2) {
-                    countButton = 1;
-                    jobCountNajatiy = 0;
-                }// друге натискання
-                knopkyNajali = true;
-                buffer[countButton] = nArr[i];// якщо кнопка 8, то і запише 8
-                if (countButton == 0) {
-                    prevIndexI = (int) i / numberOfColums;
-                    prevIndexJ = (int) i % numberOfColums;
-                    previousI = prevIndexI * numberOfColums + prevIndexJ;
-                    arrayButton[i].hide();
-                    button1 = i;
-                }
-                if (countButton == 1) {
-                    button2 = i;
-                    IndexDiff = button2 - button1;
-                    if (IndexDiff < 0) {
-                        IndexDiff = IndexDiff * (-1);// Робимо різницю ЗАВЖДИ
-                        // додатньою
-                        bigger = button1;
-                        smaller = button2;
-                    } else {
-                        bigger = button2;
-                        smaller = button1;
-                    }
-
-                }
-                if (IndexDiff == 1) {
-                    zakreslennya = Sravnenie();
-                }
-                if (IndexDiff > 1) {// тобто це не одна й та ж кнопка
-                    if (IndexDiff % numberOfColums == 0) {// якщо різниця кратна
-                        // 10 (тобто вони в
-                        // одному стовпці)
-
-                        if (IndexDiff == numberOfColums) {// і рівна стовпцям -
-                            // це сусіди.
-                            zakreslennya = Sravnenie();
-                        }
-                        // інакше
-                        if (IndexDiff > numberOfColums) {// і більша за стовпці,
-                            // то вони можуть
-                            // бути сусідами
-                            // лише тоді, коли
-                            // всі інші елементи
-                            // цього стовпця між
-                            // ними рівні нулю
-                            temp = bigger;
-                            while (temp != smaller + numberOfColums) {// десятки
-                                // всюди
-                                // замінити
-                                // на
-                                // cols!!!
-                                temp -= numberOfColums;
-                                if (nArr[temp] != 0) {
-                                    break;
-                                }
-                                if (temp == smaller + numberOfColums) {// це
-                                    // значить,
-                                    // шо
-                                    // цикл
-                                    // повністю
-                                    // виконався
-                                    zakreslennya = Sravnenie();
-                                }
-                            }
-                        }
-                    }
-                    // різниця більша одиниці, але не кратна 10
-                    // if(IndexDiff%numberOfColums!=0){
-                    else {// це означає, шо вони в одному рядку
-                        temp = bigger;
-                        while (temp != smaller + 1) {
-                            temp--;
-                            if (nArr[temp] != 0) {
-                                break;
-                            }
-                            if (temp == smaller + 1) {// це значить, шо цикл
-                                // повністю виконався
-                                zakreslennya = Sravnenie();
-                            }
-                        }
-                    }
-                }
-
-                msg = Integer.toString(i);
-
-                if (zakreslennya == true) {
-                    arrayButton[i].show();
-                    arrayButton[previousI].show();
-                    arrayButton[i].setVisible(false);// i=173,indexI=17,IndexJ=3.
-                    // 17*10+3==173//IndexI*numberOfColums+IndexJ
-                    arrayButton[previousI].setVisible(false);// prevIndexI*numberOfColums+prevIndexJ
-                    nArr[i] = 0;
-                    nArr[previousI] = 0;
-                }
-
-                if ((zakreslennya == false) && (countButton == 1)) {
-                    arrayButton[previousI].show();
-                    arrayButton[i].show();
-                    arrayButton[previousI].setVisible(true);
-                    arrayButton[i].setVisible(true);
-                }
-            }
-
-            if (knopkyNajali == true) {
-                break;
-            }
-        }
-    }
-
-
-    boolean Sravnenie() {
-        if (nArr[button1] == nArr[button2]) {
-            return true;// System.out.println("Однакові");
-        }
-        if (nArr[button1] + nArr[button2] == 10) {
-            return true;// System.out.println("Десяточка");
-        } else
-            return false;
-    }
-
-
-
-    static void ButtonsPainter(GameFieldWindow obj,int razmerMassiva) {
+    void ButtonsPainter(GameFieldWindow obj,int razmerMassiva) {
         LogicMainWindow lmw = new LogicMainWindow();
-        lmw.CreateArrayOfButtons(razmerMassiva,arrayButton,nArr);
+        lmw.createArrayOfButtons(razmerMassiva,arrayButton);
 
         // Кнопка витиснута
         Border raisedbevel = BorderFactory.createRaisedBevelBorder();
@@ -194,10 +37,6 @@ class GameFieldWindow extends JFrame implements ActionListener {
         }
     }
 
-
-
-
-
     void workWithMainWindow(GameFieldWindow obj) { // Налаштування вікна
         // windFuck.setBounds(10, 10, 1350, 300);
         obj.setExtendedState(MAXIMIZED_BOTH);
@@ -207,7 +46,20 @@ class GameFieldWindow extends JFrame implements ActionListener {
         obj.setVisible(true);
     }
 
+
+    // Обробка натискання клавіш
+    public void actionPerformed(ActionEvent actionEvent) {
+        // Обробка натискання клавіш вікна
+        for (int i = 0; i < razmerMassiva; i++) {
+            if (actionEvent.getSource() == arrayButton[i]) {
+                LogicMainWindow lmw = new LogicMainWindow();
+                lmw.disappear(arrayButton, i, numberOfColums);
+            }
+        }
+    }
 }
+
+
 
 class WindowAdapt extends WindowAdapter {
     public void windowClosing(WindowEvent ev) {
--- src/LogicMainWindow.java
+++ src/LogicMainWindow.java
@@ -1,17 +1,23 @@
 import javax.swing.*;
 
 public class LogicMainWindow {
+    int firstNumber=0,secondNumber=0;
+    int click;
+    int bigger,smaller;
+    int[] nArr=new int[181];
 
-    void CreateArrayOfButtons(int lenghtArray, JButton[] array,int[] numericalArray) {
-        CreateNumericArray(numericalArray);
+
+    void createArrayOfButtons(int lenghtArray, JButton[] array) {
+        createNumericArray(nArr);
 
         for (int i = 0; i < lenghtArray; i++) {
-            JButton button = new JButton("" +  numericalArray[i]);
+            JButton button = new JButton("" +  nArr[i]);
             array[i] = button;
         }
     }
 
-    void CreateNumericArray(int[] numericalArray) {
+    private void createNumericArray(int[] numericalArray) {
+
         int arrayCounter = 0;
         // Проганяємо всі числа від 1 до 100;
         for (int numbers = 1; numbers < 101; numbers++) {// numbers < 101
@@ -29,6 +35,116 @@ public class LogicMainWindow {
             if (numbers == 100) {
                 numericalArray[arrayCounter] = 1;
             }
+        }
+    }
+
+    void disappear(JButton [] arrayButton, int iterator, int numberOfColums){
+        int flagOfZero=0;
+
+        click++;
+        if (click==1){
+            arrayButton[iterator].hide();
+            firstNumber=iterator;
+            System.out.println("First: "+firstNumber);
+        }
+        if (click!=1){
+            secondNumber=iterator;
+            System.out.println("Second: "+secondNumber);
+            click=0;
+            System.out.println();
+
+
+            if ((nArr[firstNumber]==nArr[secondNumber])||(nArr[firstNumber]+nArr[secondNumber]==10)){
+                //Кнопки в одному рядку
+                if (firstNumber/numberOfColums==secondNumber/numberOfColums){
+                    // Якщо кнопки один біля одного
+                    if(Math.abs(firstNumber-secondNumber)==1){
+                        arrayButton[iterator].hide();
+                        nArr[firstNumber]=0;
+                        nArr[secondNumber]=0;
+                    }
+                    //Якщо кнопки на відстаі один від одного
+                    else if(Math.abs(firstNumber-secondNumber)!=1){
+                        biggerAndSmaller(firstNumber,secondNumber);
+                        // Умова завжди більша на 1 від дійсності
+                        int condition = bigger-smaller;
+                        // условия проверки значений между выбраными клавишами
+                        for (int i=0;i<condition;i++){
+                            if (nArr[smaller+i]==0){
+                                flagOfZero++;
+                                System.out.println(flagOfZero);
+                            }
+                        }
+                        //Якщо флаг дорівнює умові, то ховаємо і занудюємо
+                        if (flagOfZero==condition-1){
+                            arrayButton[iterator].hide();
+                            nArr[firstNumber]=0;
+                            nArr[secondNumber]=0;
+                            flagOfZero=0;
+                        }
+                        else{
+                            arrayButton[firstNumber].show();
+                        }
+                    }
+                    else {
+                        arrayButton[firstNumber].show();
+                    }
+                }
+
+
+                //Для стовбіц
+                else if (firstNumber%numberOfColums==secondNumber%numberOfColums){
+                    // Якщо поруч знаходяться значення по вертикалі
+                    if (Math.abs(firstNumber-secondNumber)==numberOfColums){
+                        arrayButton[iterator].hide();
+                        nArr[firstNumber]=0;
+                        nArr[secondNumber]=0;
+                    }
+                    // Коли значення знаходяться по вертикалі на відстані декілької рядків
+                    else{
+                        biggerAndSmaller(firstNumber,secondNumber);
+                        System.out.println(bigger+" "+smaller);
+
+                        // Умова завжди більша на 1 від дійсності
+                        int condition = (bigger-smaller)/numberOfColums;
+                        // условия проверки значений между выбраными клавишами
+                        for (int i=0;i<condition;i++){
+                            if (nArr[smaller+numberOfColums*i]==0){
+                                flagOfZero++;
+                                System.out.println(flagOfZero);
+                            }
+                        }
+                        //Якщо флаг дорівнює умові, то ховаємо і занудюємо
+                        if (flagOfZero==condition-1){
+                            arrayButton[iterator].hide();
+                            nArr[firstNumber]=0;
+                            nArr[secondNumber]=0;
+                            flagOfZero=0;
+                        }
+                        else{
+                            arrayButton[firstNumber].show();
+                        }
+                    }
+                }
+                else {
+                    arrayButton[firstNumber].show();
+                }
+            }
+            else {
+                arrayButton[firstNumber].show();
+            }
+
+        }
+    }
+
+    private void biggerAndSmaller(int first, int second){
+        if(first>second){
+            bigger=first;
+            smaller=second;
+        }
+        else {
+            bigger=second;
+            smaller=first;
         }
     }
 }
